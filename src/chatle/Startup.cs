b81f7382437cd45e67d14358882d4992ebcd10ba
Olivier Lefebvre <olivier.lefebvre@live.com>
2014-11-19 17:46:20 +0100

    Merge old config startup and layout

--- src/chatle/Startup.cs
+++ src/chatle/Startup.cs
@@ -1,91 +1,119 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
-using Microsoft.AspNet.Diagnostics.Entity;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Routing;
-using Microsoft.AspNet.Security.Cookies;
 using Microsoft.Data.Entity;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using chatle.Models;
+using Chat.CMACGM.Models;
+using Microsoft.Data.Entity.Redis.Extensions;
 
-namespace chatle
+namespace Chat.CMACGM
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        enum DBEngine
         {
-            // Setup configuration sources.
+            SqlServer,
+            InMemory,
+            Redis,
+            SQLite
+        }
+
+        public Startup()
+        {
+            /* 
+            * Below code demonstrates usage of multiple configuration sources. For instance a setting say 'setting1' is found in both the registered sources, 
+            * then the later source will win. By this way a Local config can be overridden by a different setting while deployed remotely.
+            */
             Configuration = new Configuration()
                 .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
+                .AddEnvironmentVariables(); //All environment variables in the process's context flow in as configuration values.
         }
 
-        public IConfiguration Configuration { get; set; }
+        public IConfiguration Configuration { get; private set; }
 
-        // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add EF services to the services container.
-            services.AddEntityFramework(Configuration)
-                .AddSqlServer()
-                .AddDbContext<ApplicationDbContext>();
+            var builder = services.AddEntityFramework();
 
-            // Add Identity services to the services container.
-            services.AddDefaultIdentity<ApplicationDbContext, ApplicationUser, IdentityRole>(Configuration);
+            var dbEngine = (DBEngine)Enum.Parse(typeof(DBEngine), Configuration.Get("DatabaseEngine"));
+            switch (dbEngine)
+            {
+                case DBEngine.InMemory:
+                    builder.AddInMemoryStore();
+                    break;
+                case DBEngine.SQLite:
+                    builder.AddSQLite();
+                    break;
+                case DBEngine.SqlServer:
+                    builder.AddSqlServer();
+                    break;
+                case DBEngine.Redis:
+                    builder.AddRedis();
+                    break;
+                default:
+                    throw new InvalidOperationException("Database engine unsupported");
+            }
 
-            // Add MVC services to the services container.
+            builder.AddDbContext<ApplicationDbContext>(options =>
+            {
+                switch (dbEngine)
+                {
+                    case DBEngine.InMemory:
+                        options.UseInMemoryStore(true);
+                        break;
+                    case DBEngine.SqlServer:
+                        options.UseSqlServer(Configuration.Get("Data:DefaultConnection:ConnectionString"));
+                        break;
+                    case DBEngine.SQLite:
+                        options.UseSQLite(Configuration.Get("Data:DefaultConnection:ConnectionString"));
+                        break;
+                    case DBEngine.Redis:
+                        int port;
+                        int database;
+                        if (!int.TryParse(Configuration.Get("Data:Redis:Port"), out port))
+                            port = 6379;
+                        int.TryParse(Configuration.Get("Data: Redis:Database"), out database);
+
+                        options.UseRedis(Configuration.Get("Data:Redis:Hostname"), port, database);
+                        break;
+                }
+            });
+
+            services.Configure<IdentityDbContextOptions>(options =>
+            {
+                options.CreateDatabase = Configuration.Get("CreateDatabase").ToLower() == "true";
+            });
+
+            services.AddDefaultIdentity<ApplicationDbContext, ApplicationUser, IdentityRole>(Configuration.GetSubKey("Identity"), options =>
+            {
+                options.SecurityStampValidationInterval = TimeSpan.FromMinutes(20);
+            });
+
             services.AddMvc();
 
-            // Uncomment the following line to add Web API servcies which makes it easier to port Web API 2 controllers.
-            // You need to add Microsoft.AspNet.Mvc.WebApiCompatShim package to project.json
-            // services.AddWebApiConventions();
-
+            services.AddSignalR(options => options.Hubs.EnableDetailedErrors = true);
         }
 
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        public void Configure(IApplicationBuilder app)
         {
-            // Configure the HTTP request pipeline.
-            // Add the console logger.
-            loggerfactory.AddConsole();
-
-            // Add the following to the request pipeline only in development environment.
-            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
-            {
-                app.UseBrowserLink();
-                app.UseErrorPage(ErrorPageOptions.ShowAll);
-                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
-            }
-
-            // Add static files to the request pipeline.
-            app.UseStaticFiles();
-
-            // Add cookie-based authentication to the request pipeline.
-            app.UseIdentity();
-
-            // Add MVC to the request pipeline.
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseBrowserLink()
+                .UseErrorPage()
+                .UseStaticFiles()
+                .UseIdentity()
+                .UseMvc(routes =>
+                {
+                    routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new { controller = "Home", action = "Index" });
 
-                // Uncomment the following line to add a route for porting Web API 2 controllers.
-                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
-            });
+                    routes.MapRoute(
+                        name: "api",
+                        template: "{controller}/{id?}");
+                })
+                .UseSignalR();
         }
     }
 }
