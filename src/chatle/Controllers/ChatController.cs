2676bff928290f3ec4d03551d531e891b67ed104
Olivier Lefebvre <olivier.lefebvre@live.com>
2014-11-21 18:20:17 +0100

    Add SendMessage

--- src/chatle/Controllers/ChatController.cs
+++ src/chatle/Controllers/ChatController.cs
@@ -1,7 +1,10 @@
-﻿using ChatLe.Models;
+﻿using ChatLe.Hubs;
+using ChatLe.Models;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.SignalR.Infrastructure;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNet.SignalR;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,15 +12,25 @@ namespace ChatLe.Controllers
 {
     public class ChatController : Controller
     {
-        ApplicationDbContext _dbContext;
-        public ChatController(ApplicationDbContext dbContext)
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IHubContext _hub;
+
+        public ChatController(ApplicationDbContext dbContext, IConnectionManager manager)
         {
             _dbContext = dbContext;
+            _hub = manager.GetHubContext<ChatHub>();
         }
         // GET: /<controller>/
+        [HttpGet("messages")]
         public IEnumerable<Message> Get(string id)
         {
             return _dbContext.Messages.Where(x => (x.From == id && x.To ==Context.User.Identity.Name) || (x.To == id && x.From == Context.User.Identity.Name)).OrderByDescending(x => x.Date);
+        }
+
+        [HttpPost("message")]
+        public void SendMessage(Message message)
+        {
+            _hub.Clients.Group(message.To).messageReceived(message.Text);
         }
     }
 }
