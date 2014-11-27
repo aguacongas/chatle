c711a8d394049f2d545d5bf2e138a37f1a608513
Olivier Lefebvre <olivier.lefebvre@live.com>
2014-11-27 17:30:34 +0100

    Create manager tests

---
+++ test/ChatLe.Repository.Text/ChatManagerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using System;
+using Xunit;
+
+namespace ChatLe.Repository.Text
+{
+    public class ChatManagerTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            var services = TestHelpers.GetServicesCollection();
+            using (var context = new DbContext(services.BuildServiceProvider()))
+            {
+                var store = new ChatManager<UserTest>(context);
+            }
+        }
+    }
+}
