2463646272a2af78cdbe520c799cbcf62db9755c
Olivier Lefebvre <olivier.lefebvre@live.com>
2014-12-15 18:48:22 +0100

    Fix files shortcuts

---
+++ src/chatle/HttpUtitity/RemoveResponseHeadersBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Builder;
+
+namespace ChatLe.HttpUtility
+{
+    public static class RemoveResponseHeaderBuilderExtensions
+    {
+        public static IApplicationBuilder UseRemoveResponseHeaders(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RemoveResponseHeadersMiddleware>();
+            return app;
+        }
+
+    }
+}
