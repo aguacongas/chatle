1f8cb6e5b3493f2a24a4501735945c7542101526
Olivier Lefebvre <olivier.lefebvre@live.com>
2014-12-27 15:40:52 +0100

    Add after build event

--- src/chatle/gruntfile.js
+++ src/chatle/gruntfile.js
@@ -1,4 +1,5 @@
-﻿// This file in the main entry point for defining grunt tasks and using grunt plugins.
+﻿/// <binding AfterBuild='uglify:beauty_target' />
+// This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
 module.exports = function (grunt) {
