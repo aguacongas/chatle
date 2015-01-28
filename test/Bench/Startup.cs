557336ca828305e206a50ff708325c1545b9c7f5
Olivier Lefebvre <olivier.lefebvre@live.com>
2015-01-28 10:04:46 +0100

    Add bench project

---
+++ test/Bench/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace Bench
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                var payload = "Bench!";
+                context.Response.ContentLength = payload.Length;
+                await context.Response.WriteAsync(payload);
+            });
+        }
+    }
+}
