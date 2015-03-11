e1c7be8c87738516c2a2ad90c42b3e532fcee8a2
Olivier Lefebvre <aguacongas@gmail.com>
2015-03-11 22:30:32 +0100

    Update appveyorBuild.cmd

--- appveyorBuild.cmd
+++ appveyorBuild.cmd
@@ -2,7 +2,7 @@ set PATH=%USERPROFILE%/.k/bin;C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 set KRE_HOME=%USERPROFILE%/.k
 call kvm upgrade
 call git submodule update --init --recursive
+cd %APPVEYOR_BUILD_FOLDER%\EntityFramework
+call git checkout beta3
+call git submodule update --init --recursive
 call kpm restore %APPVEYOR_BUILD_FOLDER%
-if %ERRORLEVEL% NEQ 0 (
-   exit /b 0
-)
