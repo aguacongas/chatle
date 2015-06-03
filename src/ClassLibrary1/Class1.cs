dc160319ec2dc9b6c237e02ddfc207781eef846b
Olivier Lefebvre <olivier.lefebvre@live.com>
2015-06-03 15:38:24 +0100

    reopen issue aspnet/EntityFramework#2253

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ClassLibrary1
+{
+    public class User:IdentityUser
+    {
+
+    }
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1: IdentityDbContext<User>
+    {
+        public Class1()
+        {
+        }
+    }
+}
