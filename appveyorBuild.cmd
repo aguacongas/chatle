34339716837549ff4024cc3be23df3deaed8c2a3
Olivier Lefebvre <aguacongas@gmail.com>
2015-12-27 21:36:41 +0100

    update appveyor build to install gulp

--- appveyorBuild.cmd
+++ appveyorBuild.cmd
@@ -1,4 +1,5 @@
 call npm install -g bower
+call npm install -g gulp
 call dnvm install latest -a x64 -r coreclr -Alias core
 call dnvm install latest -a x64 -r clr -Alias default -p
 call dnu restore --quiet
