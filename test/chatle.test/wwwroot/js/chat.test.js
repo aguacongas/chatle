d79e336fa75b487cfd5deec81e072e7b63b80979
Olivier Lefebvre <aguacongas@gmail.com>
2015-12-31 00:55:28 +0100

    start adding js unit tests

---
+++ test/chatle.test/wwwroot/js/chat.test.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../../../src/chatle/wwwroot/lib/knockout/dist/knockout.js" />
+/// <reference path="../../../../src/chatle/wwwroot/js/chat.js" />
+
+describe("chat test suite", function () {
+	it("ko must be defined", function () {
+		expect(ko).toBeDefined();
+	});
+
+	it("chatLe must be defined", function () {
+		expect(chatLe).toBeDefined();
+	});
+
+	it("chatLe.userName must be defined", function () {
+		expect(chatLe.userName).toBeDefined();
+	});
+
+	it("chatLe.chatAPI must be defined", function () {
+		expect(chatLe.chatAPI).toBeDefined();
+	});
+
+	it("chatLe.chatAPI must be defined", function () {
+		expect(chatLe.convAPI).toBeDefined();
+	});
+
+	it("chatLe.userAPI must be defined", function () {
+		expect(chatLe.userAPI).toBeDefined();
+	});
+
+	it("chatLe.init must be defined", function () {
+		expect(chatLe.init).toBeDefined();
+	});
+});
