8c297ef4f890ff2e76b60cabc35a79e50dc23d3d
Olivier Lefebvre <aguacongas@gmail.com>
2015-12-31 13:30:43 +0100

    add js tests

--- src/chatle/wwwroot/js/chat.js
+++ src/chatle/wwwroot/js/chat.js
@@ -1,48 +1,103 @@
 var chatLe = chatLe || {};
-
-!function () {
+/**
+  * @desc chat management class
+  * @author Olivier Lefebvre
+*/
+(function () {
 	"use strict";
+
 	chatLe = function () {
+		// settings
 		var settings = {
 			userName: null,
 			chatAPI: null,
 			convAPI: null,
 			userAPI: null,
 			init: null
-		}, logout = function (reason) {
-			$("#reasonHidden").val(reason), $("#logoutForm").submit();
-		}, VM = function () {
+		};
+
+		/**
+		  * @desc logout on error
+		  * @param String reason, the error
+		*/
+		var logout = function (reason) {
+			$('#reasonHidden').val(reason);
+			$('#logoutForm').submit();
+		};
+
+		/**
+		  * @desc main view model constructor
+		*/
+		var VM = function () {
+			/**
+			  * @desc get or create a one to one conversation with the user and retrieve messages
+			  * @param User user
+			*/
 			var showMessages = function (user) {
 				var conv;
 				$.each(this.conversations(), function (index, c) {
 					var attendees = c.attendees();
-					if (2 === attendees.length) for (var i = 0; i < attendees.length; i++) {
-						var a = attendees[i];
-						if (a.UserId === user.Id) return conv = c, !1;
+					if (attendees.length === 2) {
+						for (var i = 0; i < attendees.length; i++) {
+							var a = attendees[i];
+							if (a.UserId === user.Id) {
+								conv = c;
+								return false;
+							}
+						}
 					}
-				}), conv ? conv.getMessages() : (conv = new ConversationVM({
-					Id: null,
-					Attendees: [{
-						ConversattionId: null,
-						UserId: user.Id
-					}, {
-						ConversattionId: null,
-						UserId: settings.userName
-					}],
-					Messages: null
-				}), this.conversations.unshift(conv)), this.currentConv(conv);
-			}, messageReceived = function (data) {
-				if (data) {
-					var conv;
-					$.each(this.conversations(), function (index, c) {
-						return c.id === data.ConversationId ? (conv = c, !1) : void 0;
-					}), conv && conv.messages.unshift(data);
+				});
+				if (conv) {
+					conv.getMessages();
+				} else {
+					conv = new ConversationVM(
+						{
+							Id: null,
+							Attendees: [{ ConversattionId: null, UserId: user.Id }, { ConversattionId: null, UserId: settings.userName }],
+							Messages: null
+						})
+					this.conversations.unshift(conv);
 				}
-			}, joinConversation = function (data) {
-				data && (this.conversations.remove(function (conv) {
+				this.currentConv(conv);
+			};
+			/**
+			  * @desc add the message received in the conversation having id = to
+			  * @param Message data, the message
+			*/
+			var messageReceived = function (data) {
+				if (!data) {
+					return;
+				}
+				var conv;
+				$.each(this.conversations(), function (index, c) {
+					if (c.id === data.ConversationId) {
+						conv = c;
+						return false;
+					}
+				});
+				if (!conv) {
+					return;
+				}
+				conv.messages.unshift(data);
+			};
+			/**
+			  * @desc add the current user in a new conversation
+			  * @param Conv data, the conversation model
+			*/
+			var joinConversation = function (data) {
+				if (!data) {
+					return;
+				}
+				this.conversations.remove(function (conv) {
 					return conv.id === data.Id;
-				}), this.conversations.unshift(new ConversationVM(data)));
-			}, showConversation = function (conv) {
+				});
+				this.conversations.unshift(new ConversationVM(data));
+			};
+			/**
+			  * @desc set the conv as the current conversation
+			  * @param ConversationVM conv, the conversation model
+			*/
+			var showConversation = function (conv) {
 				this.currentConv(conv);
 			};
 			return {
@@ -55,49 +110,67 @@ var chatLe = chatLe || {};
 				joinConversation: joinConversation,
 				showConversation: showConversation
 			};
-		}, viewModel = new VM(), ConversationVM = function (conv) {
-			for (var sendMessage = function (textBoxId) {
-                var textBox = $(textBoxId), message = textBox.val();
-                textBox.val("");
-                var self = this;
-                self.id ? $.ajax(settings.chatAPI, {
-				data: {
-				to: self.id,
-				text: message
-			},
-				type: "POST"
-			}).done(function () {
-                    self.messages.unshift({
-				From: settings.userName,
-				Text: message
-			});
-			}).fail(function (data) {
-                    logout(data);
-			}) : $.ajax(settings.convAPI, {
-				data: {
-				to: self.attendees()[0].UserId,
-				text: message
-			},
-				type: "POST"
-			}).done(function (data) {
-                    self.id = data, self.messages.unshift({
-				From: settings.userName,
-				Text: message
-			});
-			}).fail(function (data) {
-                    logout(data);
-			});
-			}, getMessages = function () {
-                var self = this;
-                $.getJSON(settings.chatAPI + "/" + this.id).done(function (data) {
-                    self.messages(data);
-			}).fail(function (data) {
-                    logout(data);
-			});
-			}, title = "", attendees = conv.Attendees, i = 0; i < attendees.length; i++) {
+		};
+
+		var viewModel = new VM();
+		/**
+		  * @desc conversation view model constructor
+		*/
+		var ConversationVM = function (conv) {
+
+			/**
+			  * @desc send a new message in the conversation
+			  * @param String textBoxId, the id of the textbox containing the message to send
+			*/
+			var sendMessage = function (textBoxId) {
+				var textBox = $(textBoxId);
+				var message = textBox.val();
+				textBox.val('');
+				var self = this;
+				if (!self.id) {
+					$.ajax(settings.convAPI, {
+						data: { to: self.attendees()[0].UserId, text: message },
+						type: "POST"
+					}).done(function (data) {
+						self.id = data;
+						self.messages.unshift({ From: settings.userName, Text: message });
+					}).fail(function (data) {
+						logout(data);
+					});;
+				} else {
+					$.ajax(settings.chatAPI, {
+						data: { to: self.id, text: message },
+						type: "POST"
+					}).done(function () {
+						self.messages.unshift({ From: settings.userName, Text: message });
+					}).fail(function (data) {
+						logout(data);
+					});;
+				}
+			};
+			/**
+			  * @desc get messages for the conversation
+			*/
+			var getMessages = function () {
+				var self = this;
+				$.getJSON(settings.chatAPI + '/' + this.id)
+					.done(function (data) {
+						self.messages(data);
+					}).fail(function (data) {
+						logout(data);
+					});
+			};
+
+			var title = '';
+			var attendees = conv.Attendees;
+
+			for (var i = 0; i < attendees.length; i++) {
 				var attendee = attendees[i];
-				attendee.UserId !== settings.userName && (title += attendee.UserId + " ");
+				if (attendee.UserId !== settings.userName) {
+					title += attendee.UserId + ' ';
+				}
 			}
+
 			return {
 				id: conv.Id,
 				title: ko.observable(title),
@@ -106,49 +179,119 @@ var chatLe = chatLe || {};
 				sendMessage: sendMessage,
 				getMessages: getMessages
 			};
-		}, init = function (debug) {
+		};
+
+		var init = function (debug) {
+			// only for debug
 			$.connection.hub.logging = debug;
+			// get the signalR hub named 'chat'
 			var chatHub = $.connection.chat;
-			viewModel.userName = settings.userName, ko.applyBindings(viewModel), chatHub.client.userConnected = function (user) {
+
+			viewModel.userName = settings.userName;
+			// apply the view model binding
+			ko.applyBindings(viewModel);
+
+			/**
+			  * @desc callback when a new user connect to the chat
+			  * @param User user, the connected user
+			*/
+			chatHub.client.userConnected = function (user) {
 				console.log("Chat Hub newUserConnected " + user.Id);
 				var users = viewModel.users;
 				users.remove(function (u) {
 					return u.Id === user.Id;
-				}), users.unshift(user);
-			}, chatHub.client.userDisconnected = function (id) {
-				console.log("Chat Hub userDisconnected " + id), viewModel.users.remove(function (u) {
+				});
+				users.unshift(user);
+			};
+			/**
+			  * @desc callback when a new user disconnect the chat
+			  * @param id, the disconnected user id
+			*/
+			chatHub.client.userDisconnected = function (id) {
+				console.log("Chat Hub userDisconnected " + id);
+				viewModel.users.remove(function (u) {
 					return u.Id === id;
 				});
-			}, chatHub.client.messageReceived = function (data) {
+			};
+			/**
+			  * @desc callback when a message is received
+			  * @param String to, the conversation id
+			  * @param Message data, the message
+			*/
+			chatHub.client.messageReceived = function (data) {
 				viewModel.messageReceived(data);
-			}, chatHub.client.joinConversation = function (data) {
-				console.log("join conversation " + JSON.stringify(data)), viewModel.joinConversation(data);
-			}, $.connection.hub.stateChanged(function (change) {
-				var oldState = null, newState = null;
-				for (var state in $.signalR.connectionState) $.signalR.connectionState[state] === change.oldState && (oldState = state),
-                $.signalR.connectionState[state] === change.newState && (newState = state);
-				console.log("Chat Hub state changed from " + oldState + " to " + newState);
-			}), $.connection.hub.reconnected(function () {
-				console.log("Chat Hub reconnect");
-			}), $.connection.hub.error(function (err) {
-				console.log("Chat Hub error"), logout(err);
-			}), $.connection.hub.disconnected(function () {
-				console.log("Chat Hub disconnected"), logout("disconnected");
-			}), $.connection.hub.start().done(function () {
-				console.log("Chat Hub started"), $.getJSON(settings.userAPI).done(function (data) {
-					viewModel.users(data.Users);
-				}).fail(function (data) {
-					logout(data);
-				}), $.getJSON(settings.chatAPI).done(function (data) {
-					data && $.each(data, function (index, conv) {
-						viewModel.conversations.unshift(new ConversationVM(conv));
-					});
-				}).fail(function (data) {
-					logout(data);
+			};
+			/**
+			  * @desc callback when a new conversation is create on server
+			  * @param Conv data, the conversation model
+			*/
+			chatHub.client.joinConversation = function (data) {
+				console.log("join conversation " + JSON.stringify(data));
+				viewModel.joinConversation(data);
+			};
+
+			if (debug) {
+				// for debug only, callback on connection state change
+				$.connection.hub.stateChanged(function (change) {
+					var oldState = null,
+						newState = null;
+
+					for (var state in $.signalR.connectionState) {
+						if ($.signalR.connectionState[state] === change.oldState) {
+							oldState = state;
+						}
+						if ($.signalR.connectionState[state] === change.newState) {
+							newState = state;
+						}
+					}
+
+					console.log("Chat Hub state changed from " + oldState + " to " + newState);
 				});
+				// for debug only, callback on connection reconnect
+				$.connection.hub.reconnected(function () {
+					console.log("Chat Hub reconnect");
+				});
+			}
+			// callback on connection error
+			$.connection.hub.error(function (err) {
+				console.log("Chat Hub error");
+				logout(err);
 			});
+			// callback on connection disconnect
+			$.connection.hub.disconnected(function () {
+				console.log("Chat Hub disconnected");
+				logout('disconnected');
+			});
+			// start the connection
+			$.connection.hub.start()
+				.done(function () {
+					console.log("Chat Hub started");
+					// get connected users
+					$.getJSON(settings.userAPI)
+						.done(function (data) {
+							viewModel.users(data.Users);
+						}).fail(function (data) {
+							logout(data);
+						});
+
+					// get user conversations
+					$.getJSON(settings.chatAPI)
+						.done(function (data) {
+							if (!data) {
+								return;
+							}
+							$.each(data, function (index, conv) {
+								viewModel.conversations.unshift(new ConversationVM(conv));
+							})
+						}).fail(function (data) {
+							logout(data);
+						});
+				});
 		};
-		return settings.init = init, settings;
+
+		settings.init = init;
+		return settings;
 	};
-}(), chatLe = chatLe();
-//# sourceMappingURL=chat.js.map
+
+})();
+chatLe = chatLe();
--- test/chatle.test/wwwroot/js/chat.test.js
+++ test/chatle.test/wwwroot/js/chat.test.js
@@ -1,10 +1,9 @@
-﻿/// <reference path="../../../../src/chatle/wwwroot/lib/knockout/dist/knockout.js" />
+﻿/// <reference path="../../../../src/chatle/wwwroot//lib/jquery/dist/jquery.js" />
+/// <reference path="../../../../src/chatle/wwwroot//lib/signalr/jquery.signalR.js" />
+/// <reference path="../../../../src/chatle/wwwroot/lib/knockout/dist/knockout.js" />
 /// <reference path="../../../../src/chatle/wwwroot/js/chat.js" />
 
 describe("chat test suite", function () {
-	it("ko must be defined", function () {
-		expect(ko).toBeDefined();
-	});
 
 	it("chatLe must be defined", function () {
 		expect(chatLe).toBeDefined();
@@ -28,5 +27,63 @@ describe("chat test suite", function () {
 
 	it("chatLe.init must be defined", function () {
 		expect(chatLe.init).toBeDefined();
+	});
+
+	describe("initialize chat", function () {
+
+		beforeEach(function () {
+			// mock connection
+			$.connection.chat = {
+				client: {}
+			};
+			$.connection.hub = {
+				stateChanged: function (onStateChange) { },
+				reconnected: function (onReconnect) { },
+				error: function (onError) { },
+				disconnected: function (onDisconnected) { },
+				start: function () { return { done: function () { } }}
+			};
+
+			spyOn(chatLe, 'init').and.callThrough();
+			spyOn($.connection.hub, 'stateChanged').and.callThrough();
+			spyOn($.connection.hub, 'reconnected').and.callThrough();
+			spyOn($.connection.hub, 'error').and.callThrough();
+			spyOn($.connection.hub, 'disconnected').and.callThrough();
+			// mock ko
+			ko.applyBindings = function (vm) { };
+			chatLe.init(true);
+		});
+
+		it("chatLe.init must called", function () {
+			expect(chatLe.init).toHaveBeenCalledWith(true);
+		});
+
+		it("$.connection.chat.client.userConnected must be defined", function () {
+			expect($.connection.chat.client.userConnected).toBeDefined();;
+		});
+
+		it("$.connection.chat.client.messageReceived must be defined", function () {
+			expect($.connection.chat.client.messageReceived).toBeDefined();
+		});
+
+		it("$.connection.chat.client.joinConversation must be defined", function () {
+			expect($.connection.chat.client.joinConversation).toBeDefined();
+		});
+
+		it("$.connection.hub.stateChanged must have been called in debug mode", function () {
+			expect($.connection.hub.stateChanged).toHaveBeenCalled();
+		});
+
+		it("$.connection.hub.reconnected must have been called  in debug mode", function () {
+			expect($.connection.hub.reconnected).toHaveBeenCalled();
+		});
+
+		it("$.connection.hub.error must have been called", function () {
+			expect($.connection.hub.error).toHaveBeenCalled();
+		});
+
+		it("$.connection.hub.disconnected must have been called", function () {
+			expect($.connection.hub.disconnected).toHaveBeenCalled();
+		});
 	});
 });
