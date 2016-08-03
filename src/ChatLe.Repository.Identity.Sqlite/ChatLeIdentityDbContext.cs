f2f851ad4ca2a597fa1037b7998dd36d76965981
olefebvre <olefebvre@cityway.fr>
2016-08-03 09:55:14 +0200

    Add initial migrations for Sqlite and SqlServer

---
+++ src/ChatLe.Repository.Identity.Sqlite/ChatLeIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatLe.Repository.Identity.Sqlite
+{
+    public class ChatLeIdentityDbContext: ChatLe.Models.ChatLeIdentityDbContext
+	{
+        public ChatLeIdentityDbContext()
+        {
+        }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Filename=./ChatLeDbContext.db");
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
