9af0cba8955d8939c734ef38e403cc593a7148c1
Olivier Lefebvre <aguacongas@gmail.com>
2017-03-11 19:38:24 +0100

    upgrade CI build and cleanup shade and backup

--- build.ps1
+++ build.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Powershell version: " $PSVersionTable.PSVersion
-
-function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $retries) 
+function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $retries)
 {
     while($true)
     {
@@ -21,7 +19,7 @@ function DownloadWithRetry([string] $url, [string] $downloadLocation, [int] $ret
                 Start-Sleep -Seconds 10
 
             }
-            else 
+            else
             {
                 $exception = $_.Exception
                 throw $exception
@@ -45,18 +43,18 @@ $buildFolder = ".build"
 $buildFile="$buildFolder\KoreBuild.ps1"
 
 if (!(Test-Path $buildFolder)) {
-    Write-Host "Downloading KoreBuild from $koreBuildZip"    
-    
+    Write-Host "Downloading KoreBuild from $koreBuildZip"
+
     $tempFolder=$env:TEMP + "\KoreBuild-" + [guid]::NewGuid()
     New-Item -Path "$tempFolder" -Type directory | Out-Null
 
     $localZipFile="$tempFolder\korebuild.zip"
-    
+
     DownloadWithRetry -url $koreBuildZip -downloadLocation $localZipFile -retries 6
 
     Add-Type -AssemblyName System.IO.Compression.FileSystem
     [System.IO.Compression.ZipFile]::ExtractToDirectory($localZipFile, $tempFolder)
-    
+
     New-Item -Path "$buildFolder" -Type directory | Out-Null
     copy-item "$tempFolder\**\build\*" $buildFolder -Recurse
 
@@ -66,14 +64,4 @@ if (!(Test-Path $buildFolder)) {
     }
 }
 
-# We still nuget because dotnet doesn't have support for pushing packages
-Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v3.5.0-beta2/NuGet.exe" -OutFile "$buildFolder/nuget.exe"
-
-&"$buildFolder\NuGet.exe" install GitVersion.CommandLine -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-&"$buildFolder\NuGet.exe" install OpenCover -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Version 4.6.166 -Out packages
-&"$buildFolder\NuGet.exe" install coveralls.io -ExcludeVersion -Source https://www.nuget.org/api/v2/  -Out packages
-&"$buildFolder\NuGet.exe" install ReportGenerator -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-&"$buildFolder\NuGet.exe" install Chutzpah -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-
-
-&"$buildFile" $args
+&"$buildFile" @args
--- build.sh
+++ build.sh
@@ -9,16 +9,15 @@ fi
 
 buildFolder=".build"
 buildFile="$buildFolder/KoreBuild.sh"
-#export KOREBUILD_DOTNET_VERSION="latest"
 
 if test ! -d $buildFolder; then
     echo "Downloading KoreBuild from $koreBuildZip"
-    
-    tempFolder="/tmp/KoreBuild-$(uuidgen)"    
+
+    tempFolder="/tmp/KoreBuild-$(uuidgen)"
     mkdir $tempFolder
-    
+
     localZipFile="$tempFolder/korebuild.zip"
-    
+
     retries=6
     until (wget -O $localZipFile $koreBuildZip 2>/dev/null || curl -o $localZipFile --location $koreBuildZip 2>/dev/null)
     do
@@ -30,39 +29,18 @@ if test ! -d $buildFolder; then
         echo "Waiting 10 seconds before retrying. Retries left: $retries"
         sleep 10s
     done
-    
+
     unzip -q -d $tempFolder $localZipFile
-  
+
     mkdir $buildFolder
     cp -r $tempFolder/**/build/** $buildFolder
-    
+
     chmod +x $buildFile
-    
+
     # Cleanup
-    if test ! -d $tempFolder; then
-        rm -rf $tempFolder  
+    if test -d $tempFolder; then
+        rm -rf $tempFolder
     fi
 fi
-
-nugetPath="$buildFolder/nuget.exe"
-if [ ! -f $nugetPath ]; then
-    nugetUrl="https://dist.nuget.org/win-x86-commandline/v3.5.0-beta2/NuGet.exe"
-    wget -O $nugetPath $nugetUrl 2>/dev/null || curl -o $nugetPath --location $nugetUrl 2>/dev/null
-fi
-
-nuget="mono $buildFolder/nuget.exe"
-
-$nuget install GitVersion.CommandLine -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-
-# cliversion="1.0.0-preview3-003206"
-# clifile="$buildFolder/cli.version"
-# if [ -f "$clifile" ]; then
-#     rm $clifile
-# fi
-# 
-# echo $cliversion >> $clifile
-# cp $clifile "$buildFolder/cli.version.darwin"
-# cp $clifile "$buildFolder/cli.version.unix"
-
 
 $buildFile -r $repoFolder "$@"
