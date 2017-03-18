66cab6d11ab140e61708d437f34a6a2a9c31a11c
Olivier Lefebvre <aguacongas@gmail.com>
2017-03-18 09:44:37 +0100

    install dotnet before KoreBuild

--- build.ps1
+++ build.ps1
@@ -78,6 +78,56 @@ if ("${env:CI}${env:APPVEYOR}${env:TEAMCITY_VERSION}${env:TRAVIS}" -eq "")
     $preflightClpOption=''
 }
 
+$dotnetVersionFile = ".build\cli.version"
+$dotnetChannel = "preview"
+$dotnetVersion = Get-Content $dotnetVersionFile
+$sharedRuntimeVersionFile = ".build\shared-runtime.version"
+$sharedRuntimeVersion = Get-Content $sharedRuntimeVersionFile
+
+if ($env:KOREBUILD_DOTNET_CHANNEL)
+{
+    $dotnetChannel = $env:KOREBUILD_DOTNET_CHANNEL
+}
+if ($env:KOREBUILD_DOTNET_VERSION)
+{
+    $dotnetVersion = $env:KOREBUILD_DOTNET_VERSION
+}
+
+$dotnetLocalInstallFolder = $env:DOTNET_INSTALL_DIR
+if (!$dotnetLocalInstallFolder)
+{
+    $dotnetLocalInstallFolder = "$env:LOCALAPPDATA\Microsoft\dotnet\"
+}
+
+function InstallSharedRuntime([string] $version, [string] $channel)
+{
+    $sharedRuntimePath = [IO.Path]::Combine($dotnetLocalInstallFolder, 'shared', 'Microsoft.NETCore.App', $version)
+    # Avoid redownloading the CLI if it's already installed.
+    if (!(Test-Path $sharedRuntimePath))
+    {
+        & "$PSScriptRoot\dotnet\dotnet-install.ps1" -Channel $channel -SharedRuntime -Version $version -Architecture x64
+    }
+}
+
+$newPath = "$dotnetLocalInstallFolder;$env:PATH"
+if ($env:KOREBUILD_SKIP_RUNTIME_INSTALL -eq "1")
+{
+    Write-Host "Skipping runtime installation because KOREBUILD_SKIP_RUNTIME_INSTALL = 1"
+    # Add to the _end_ of the path in case preferred .NET CLI is not in the default location.
+    $newPath = "$env:PATH;$dotnetLocalInstallFolder"
+}
+else
+{
+    # Install the version of dotnet-cli used to compile
+    & "$PSScriptRoot\.build\dotnet\dotnet-install.ps1" -Channel $dotnetChannel -Version $dotnetVersion -Architecture x64
+}
+
+if (!($env:Path.Split(';') -icontains $dotnetLocalInstallFolder))
+{
+    Write-Host "Adding $dotnetLocalInstallFolder to PATH"
+    $env:Path = "$newPath"
+}
+
 exec dotnet restore "$makeFileProj"
 exec dotnet build "$makeFileProj" -f netcoreapp1.1
 &"$buildFile" @args
