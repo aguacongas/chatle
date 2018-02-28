46ddc0cbc7681f939ecaf1171a3799509fd25eb8
Olivier Lefebvre <olivier.lefebvre@live.com>
2018-02-28 21:28:18 +0100

    add dotnet tool to crypt file

---
+++ src/dotnet-encrypt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChatLe.Cryptography.Tools
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2 || args[0] == "-h" || args[0] == "--help")
+            {
+                Console.WriteLine("donet-encrypt");
+                Console.WriteLine("\tencrypt a file that can be read using ChatLe.Cryptography.Utility.");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\tdotnet encrypt <path to the file to encryp> <secret-key>");
+                return;
+            }
+
+            using (var utility = new Utility(args[1]))
+            {
+                await utility.EncryptFile(args[0]);
+            }
+        }
+    }
+}
